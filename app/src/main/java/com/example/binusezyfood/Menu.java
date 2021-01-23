d79535e207a152ddcf0deb24f0003f71b57e4668
Krishna Ksatria Putra <ksatriaputra.krishna@gmail.com>
2021-01-23 09:34:14 +0700

    Second Commit

---
+++ app/src/main/java/com/example/binusezyfood/Menu.java
@@ -0,0 +1,47 @@
+package com.example.binusezyfood;
+
+public class Menu {
+    String name;
+    String type;
+    int stock;
+    int price;
+
+    public Menu(String name, String type, int stock, int price) {
+        this.name = name;
+        this.type = type;
+        this.stock = stock;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
